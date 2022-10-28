b447469dac2ac994531f6291246178fe89eab6ab
suthyscott <sutherlandscott21@gmail.com>
2022-10-28 10:00:38 -0600

    added active nav link effects

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,37 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react"
+import { NavLink } from "react-router-dom"
+import "./Header.css"
 
 const Header = () => {
-  return (
-    <div>
-        <NavLink to='/'>
-            Home
-        </NavLink>
-        <NavLink to='/auth'>
-            Auth
-        </NavLink>
-        <NavLink to='/add'>
-            Add Recipe
-        </NavLink>
-    </div>
-  )
+    return (
+        <nav>
+            <NavLink
+                to="/"
+                end
+                style={({ isActive }) => ({
+                    background: isActive ? "#59A5D8" : "none"
+                })}
+            >
+                Home
+            </NavLink>
+            <NavLink
+                to="/auth"
+                style={({ isActive }) => ({
+                    background: isActive ? "#59A5D8" : "none"
+                })}
+            >
+                Auth
+            </NavLink>
+            <NavLink
+                to="/add"
+                style={({ isActive }) => ({
+                    background: isActive ? "#59A5D8" : "none"
+                })}
+            >
+                Add Recipe
+            </NavLink>
+        </nav>
+    )
 }
 
 export default Header
